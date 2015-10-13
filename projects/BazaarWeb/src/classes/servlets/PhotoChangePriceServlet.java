b04cc89e3722306ad6dc6eb6408739e34648fa0e
spmvanmierlo <spm.vanmierlo@student.fontys.nl>
2015-10-13 15:54:12 +0200

    Change price of picture (UI)

---
+++ projects/BazaarWeb/src/classes/servlets/PhotoChangePriceServlet.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.servlets;
+
+import classes.domain.Picture;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author sjorsvanmierlo
+ */
+@MultipartConfig
+@WebServlet(urlPatterns = {"/PhotoChangePriceServlet"})
+public class PhotoChangePriceServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String photoIDValue = request.getParameter("photoId");
+        int photoId = Integer.parseInt(photoIDValue);
+        String newPriceValue = request.getParameter("photoPrice");
+        double newPrice = Double.parseDouble(newPriceValue);
+                
+        processRequest(request, response, Picture.updatePrice(newPrice,photoId));
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @param result
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response, boolean result)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet fotoUpload</title>");
+            out.println("</head>");
+            out.println("<body>");
+            if(result){
+                out.println("<h1>Price changed to " + request.getParameter("photoPrice") + " for photoId = " + request.getParameter("photoId") + "</h1>");
+            }else{
+                out.println("<h1>Price did not update!</h1>");
+            }
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+}
--- projects/BazaarWeb/web/pictureView.jsp
+++ projects/BazaarWeb/web/pictureView.jsp
@@ -7,6 +7,20 @@
 <%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
 
 <t:MasterPageContent title="Picture view">
+    <jsp:attribute name="script">
+        <script>
+            (function ($) {
+                $('.spinner .btn:first-of-type').on('click', function () {
+                    $('.spinner input').val(parseInt($('.spinner input').val(), 10) + 1);
+                });
+                $('.spinner .btn:last-of-type').on('click', function () {
+                    $('.spinner input').val(parseInt($('.spinner input').val(), 10) - 1);
+                });
+            })(jQuery);
+        </script>
+    </jsp:attribute>
+
+
     <jsp:body>
 
         <div class="container">
@@ -30,6 +44,21 @@
                     <form action="DeletePictureServlet" method="post" enctype="multipart/form-data">
                         <input type="hidden" value="1" name="photoId"/>
                         <button class="btn bg-maroon margin"><i class="fa fa-trash-o pr-5"></i>Delete</button> 
+                    </form>
+                    </p>
+                    
+                </div>
+                <div class="col-lg-3">
+                    <div class="title">
+                        <h3>Price</h3>
+                        <hr>
+                    </div>
+                    <p>
+                    <form action="PhotoChangePriceServlet" method="post" enctype="multipart/form-data">
+                        <input type="hidden" name="photoId" value="1"/>
+                        <input type="number" class="form-control" name="photoPrice"  id="photoPrice" min="0" max="99999" step="0.01">
+                        <br>
+                        <button class="btn bg-blue margin"><i class="fa fa-save pr-5"></i>Save</button>
                     </form>
                     </p>
                 </div>
