9a01f9a09dea2e6790d7f61b6e1e7e7c8e6f3ce5
Anton van Dijk <anton.van.dijk@live.nl>
2015-10-20 12:43:16 +0200

    Fixed error page 500 and added contextPath to ShowPictureServlet

--- projects/BazaarWeb/src/classes/servlets/ShowPictureServlet.java
+++ projects/BazaarWeb/src/classes/servlets/ShowPictureServlet.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- *
  * @author Jip
  */
 @WebServlet(name = "ShowPicture", urlPatterns = {"/ShowPictureServlet"})
@@ -25,36 +24,25 @@ public class ShowPictureServlet extends HttpServlet {
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-//        try (PrintWriter out = response.getWriter()) {
-//            /* TODO output your page here. You may use following sample code. */
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet ShowPicture</title>");
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>Servlet ShowPicture at " + request.getContextPath() + "</h1>");
-//            out.println("</body>");
-//            out.println("</html>");
-//        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -71,10 +59,10 @@ public class ShowPictureServlet extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -83,7 +71,7 @@ public class ShowPictureServlet extends HttpServlet {
         if (request.getParameter("imageCode") != null) {
             String imageCode = request.getParameter("imageCode");
 
-            response.sendRedirect("/pages/pictureView.jsp");
+            response.sendRedirect(request.getContextPath() + "/pages/pictureView.jsp");
         }
     }
 
