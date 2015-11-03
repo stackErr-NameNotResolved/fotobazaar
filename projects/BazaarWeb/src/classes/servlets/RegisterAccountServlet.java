7e592b0cb1e364a3b137cab5d8f3c82dd523ac8b
Anton van Dijk <anton.van.dijk@live.nl>
2015-11-03 12:46:50 +0100

    Fixed merge errors

--- projects/BazaarWeb/src/classes/servlets/RegisterAccountServlet.java
+++ projects/BazaarWeb/src/classes/servlets/RegisterAccountServlet.java
@@ -5,14 +5,15 @@
  */
 package classes.servlets;
 
-import classes.domain.Account;
-import java.io.IOException;
+import classes.domain.models.Account;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  *
