46ee45f989523bc4a5343ca5006f314b27ed79a1
Corpelijn <bassiecorpelijn@hotmail.com>
2015-11-10 14:11:04 +0100

    Not working locale text

--- projects/BazaarWeb/src/classes/servlets/fragments/CartServletFragment.java
+++ projects/BazaarWeb/src/classes/servlets/fragments/CartServletFragment.java
@@ -1,6 +1,7 @@
 package classes.servlets.fragments;
 
 import classes.domain.*;
+import classes.servlets.base.BaseHttpServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +12,7 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 
 @WebServlet(name = "CartServletFragment", urlPatterns = {"/CartServletFragment"})
-public class CartServletFragment extends HttpServlet {
+public class CartServletFragment extends BaseHttpServlet {
 
     private static DecimalFormat df;
 
@@ -44,21 +45,21 @@ public class CartServletFragment extends HttpServlet {
         if (crt == null) {
             writer.append("<table witdh=\"100%\"><tr><td align=\"center\">");
             writer.append("<i style=\"font-size:50pt;\" class=\"glyphicon glyphicon-shopping-cart\"></i></td></tr>");
-            writer.append("<tr><td>There are no items in your cart. Add items to your cart to view them here</td></tr></table>");
+            writer.append("<tr><td>" + getLocal(request, "cart.empty") + "</td></tr></table>");
         } else if (crt.getOverview().length == 0) {
             writer.append("<table witdh=\"100%\"><tr><td align=\"center\">");
             writer.append("<i style=\"font-size:50pt;\" class=\"glyphicon glyphicon-shopping-cart\"></i></td></tr>");
-            writer.append("<tr><td>There are no items in your cart. Add items to your cart to view them here</td></tr></table>");
+            writer.append("<tr><td>" + getLocal(request, "cart.empty") + "</td></tr></table>");
         } else {
             String styletag = " style=\"vertical-align: middle\"";
 
             writer.append("<table class=\"table table-hover\">");
             writer.append("<thead><tr>"
-                    + "<th width=\"46%\">Product</th>"
-                    + "<th width=\"" + ROW_WIDTH + "%\">Product prijs</th>"
-                    + "<th width=\"" + ROW_WIDTH + "%\">Foto prijs</th>"
-                    + "<th width=\"" + ROW_WIDTH + "%\">Aantal</th>"
-                    + "<th width=\"" + ROW_WIDTH + "%\">Totaal</th>"
+                    + "<th width=\"46%\">" + getLocal(request, "cart.product") + "</th>"
+                    + "<th width=\"" + ROW_WIDTH + "%\">" + getLocal(request, "cart.productp") + "</th>"
+                    + "<th width=\"" + ROW_WIDTH + "%\">" + getLocal(request, "cart.fotop") + "</th>"
+                    + "<th width=\"" + ROW_WIDTH + "%\">" + getLocal(request, "cart.amount") + "</th>"
+                    + "<th width=\"" + ROW_WIDTH + "%\">" + getLocal(request, "cart.total") + "</th>"
                     + "<th width=\"" + ROW_WIDTH + "%\"></th>"
                     + "<th width=\"" + ROW_WIDTH + "%\"></th>"
                     + "</tr></thead>");
@@ -68,13 +69,13 @@ public class CartServletFragment extends HttpServlet {
             for (Order o : crt.getOverview()) {
 
                 writer.append("<tr>"
-                        + "<td" + styletag + ">" + o.getItem().toString() + " + foto</td>"
+                        + "<td" + styletag + ">" + o.getItem().toString() + " + " + getLocal(request, "cart.foto") + "</td>"
                         + "<td" + styletag + ">€ " + formatDouble(o.getItem().getPrice()) + "</td>"
                         + "<td" + styletag + ">€ " + formatDouble(o.getPicture().getPrice()) + "</td>"
-                        + "<td" + styletag + "><input class=\"form-control\" id=\"aantal" + o.getId() + "\" placeholder=\"Aantal\" type=\"number\" min=\"1\" value=\"" + o.getAmount() + "\"></input></td>"
+                        + "<td" + styletag + "><input class=\"form-control\" id=\"aantal" + o.getId() + "\" placeholder=\"" + getLocal(request, "cart.amount") + "\" type=\"number\" min=\"1\" value=\"" + o.getAmount() + "\"></input></td>"
                         + "<td class=\"active\" style=\"text-align: right; vertical-align: middle\">€ " + formatDouble(o.getTotalPrice()) + "</td>"
-                        + "<td" + styletag + "><a class=\"btn btn-primary\" style=\"font-size: 8pt;\"><i class=\"glyphicon glyphicon-edit\" style=\"color: white;\"></i>   Bewerken</a></td>"
-                        + "<td" + styletag + "><a class=\"btn btn-danger\" style=\"font-size: 8pt;\"><i class=\"glyphicon glyphicon-remove\" style=\"color: white;\"></i>   Verwijderen</a></td>"
+                        + "<td" + styletag + "><a class=\"btn btn-primary\" style=\"font-size: 8pt;\"><i class=\"glyphicon glyphicon-edit\" style=\"color: white;\"></i>   " + getLocal(request, "cart.edit") + "</a></td>"
+                        + "<td" + styletag + "><a class=\"btn btn-danger\" style=\"font-size: 8pt;\"><i class=\"glyphicon glyphicon-remove\" style=\"color: white;\"></i>   " + getLocal(request, "cart.delete") + "</a></td>"
                 );
             }
 
@@ -82,7 +83,7 @@ public class CartServletFragment extends HttpServlet {
 
             writer.append("<tr><td></td><td></td><td></td>"
                     + "<td>"
-                    + "<b>Subtotaal</b><br/>BTW (21%)<br/><b>Totaal</b></td>"
+                    + "<b>" + getLocal(request, "cart.subtotal") + "</b><br/>" + getLocal(request, "cart.btw") + "<br/><b>" + getLocal(request, "cart.finaltotal") + "</b></td>"
                     + "<td class=\"active\" style=\"text-align: right\">€ " + formatDouble(crt.getTotalPrice())
                     + "<br/>€ " + formatDouble(crt.getBTW(21))
                     + "<br/>€ " + formatDouble(crt.getTotalPriceAndBTW(21))
@@ -90,13 +91,13 @@ public class CartServletFragment extends HttpServlet {
 
             writer.append("</tfoot></table>");
 
-            writer.append("<br /><br /><div class=\"col-md-9\">Wijzigingen in de aantallen worden pas zichtbaar in de prijs als er op de knop \"Opslaan\" wordt geklikt</div>");
+            writer.append("<br /><br /><div class=\"col-md-9\">" + getLocal(request, "cart.empty") + "</div>");
             writer.append("<div class=\"col-md-3\">");
             writer.append("<div class=\"col-md-2\"></div>");
             //writer.write("<input type=\"submit\"/>");
-            writer.append("<a href=\"#\" onclick=\"$(this).closest('form').submit()\"><div class=\"btn btn-info col-md-4\">Opslaan</div></a>");
+            writer.append("<a href=\"#\" onclick=\"$(this).closest('form').submit()\"><div class=\"btn btn-info col-md-4\">" + getLocal(request, "cart.save") + "</div></a>");
             writer.append("<div class=\"col-md-1\"></div>");
-            writer.append("<a href=\"payment.jsp\"><div class=\"btn btn-info col-md-4\">Betalen</div></a>");
+            writer.append("<a href=\"payment.jsp\"><div class=\"btn btn-info col-md-4\">" + getLocal(request, "cart.pay") + "</div></a>");
             writer.append("</div>");
 
         }
