425e4e6a3c33a2f7e5a223a5f2950d7b1bb6ab5c
spmvanmierlo <spm.vanmierlo@student.fontys.nl>
2015-11-10 13:50:15 +0100

    Item create and delete, inc. Test

--- projects/BazaarWeb/src/classes/domain/Item.java
+++ projects/BazaarWeb/src/classes/domain/Item.java
@@ -5,6 +5,11 @@
  */
 package classes.domain;
 
+import classes.database.DatabaseConnector;
+import classes.database.StatementResult;
+import java.io.InputStream;
+import javax.servlet.http.Part;
+
 /**
  *
  * @author Bas
@@ -34,4 +39,78 @@ public class Item {
     {
         return this.description;
     }
+    
+    /**
+     * Deletes an item in the Database
+     * @param itemId The id the item
+     * @return if the item is deleted
+     */
+    public static boolean delete(int itemId){
+        
+        boolean result = false;
+        StatementResult dbResult = null;
+        
+        if(itemId > 0){            
+            try{
+                dbResult = DatabaseConnector.getInstance().executeNonQuery("DELETE FROM item WHERE id=?", itemId);
+                result = true;
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+            
+            if(dbResult.equals(StatementResult.ERROR) || dbResult.equals(StatementResult.NO_ROWS_UPDATED))
+            {
+                result = false;                
+            }
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Creates an item in the Database
+     * @param code The image code
+     * @param description The description of the item
+     * @param price The price of the item
+     * @param isActive Is the item active
+     * @param image The image of the item
+     * @return if the item is created
+     */
+    public static boolean create(String code, String description, double price, boolean isActive, Part image){
+        boolean result = false;
+        StatementResult dbResult = null;
+        
+        int dbIsActive = 0;
+        if(isActive){
+            dbIsActive = 1;
+        }
+        
+        if(price > 0.00){
+            if(!code.isEmpty() || !description.isEmpty())
+            {
+                try{                
+                    if(image == null){
+                        //Upload without image
+                        dbResult = DatabaseConnector.getInstance().executeNonQuery("INSERT INTO item (CODE, DESCRIPTION, PRICE, ACTIVE) VALUES (?,?,?,?)", description, code, price, dbIsActive);
+
+                    }else{
+                        //Upload with image
+                        InputStream dbImage = image.getInputStream();
+                        dbResult = DatabaseConnector.getInstance().executeNonQuery("INSERT INTO item (CODE, DESCRIPTION, PRICE, ACTIVE, IMAGE) VALUES (?,?,?,?,?)", description, code, price, dbIsActive,dbImage);
+                    }
+
+                    if(!dbResult.equals(StatementResult.ERROR) || !dbResult.equals(StatementResult.NO_ROWS_UPDATED)){
+                        result = true;
+                    }
+
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            }    
+        }    
+        
+        return result;
+    }
+    
+    
 }
