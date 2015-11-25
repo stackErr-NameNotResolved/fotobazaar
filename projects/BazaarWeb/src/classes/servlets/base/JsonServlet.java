e63a2189bb6c7bd4157a9f26c3edc330d3720b4e
Anton van Dijk <anton.van.dijk@live.nl>
2015-11-25 11:08:15 +0100

    Added doGet in JsonServlet for get JSON responses.

--- projects/BazaarWeb/src/classes/servlets/base/JsonServlet.java
+++ projects/BazaarWeb/src/classes/servlets/base/JsonServlet.java
@@ -8,17 +8,50 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public abstract class JsonServlet extends BaseHttpServlet {
-    @Override
-    protected final void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    private void setJsonResponse(HttpServletResponse resp) {
         resp.setCharacterEncoding("utf-8");
         resp.setContentType("application/json");
+    }
+
+    @Override
+    protected final void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        setJsonResponse(resp);
         JsonObjectBuilder builder = Json.createObjectBuilder();
+
         doPost(req, resp, builder);
+
         resp.getWriter().write(builder.build().toString());
         resp.getWriter().flush();
     }
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp, JsonObjectBuilder builder) {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        setJsonResponse(resp);
+        JsonObjectBuilder builder = Json.createObjectBuilder();
 
+        doGet(req, resp, builder);
+
+        resp.getWriter().write(builder.build().toString());
+        resp.getWriter().flush();
+    }
+
+    /**
+     * Uses the {@link JsonObjectBuilder} to return a JSON response.
+     *
+     * @param req     Request that was received from the client's browser.
+     * @param resp    Response that has been created on the server.
+     * @param builder {@link JsonObjectBuilder} to return a JSON response.
+     */
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp, JsonObjectBuilder builder) {
+    }
+
+    /**
+     * Uses the {@link JsonObjectBuilder} to return a JSON response.
+     *
+     * @param req     Request that was received from the client's browser.
+     * @param resp    Response that has been created on the server.
+     * @param builder {@link JsonObjectBuilder} to return a JSON response.
+     */
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp, JsonObjectBuilder builder) {
     }
 }
