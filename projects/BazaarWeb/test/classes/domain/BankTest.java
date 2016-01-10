5bcc59dc7118b0168f84be1bdecf32fb432d1aed
bayahorloo <bayahorloo@github.com>
2016-01-10 21:27:30 +0100

    BankTest + minor UI changes
    Discarded UI elements for online payment

---
+++ projects/BazaarWeb/test/classes/domain/BankTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Baya
+ */
+public class BankTest {
+
+    BankAccount bank1;
+    BankAccount bank2;
+    BankAccount bank3;
+
+    public BankTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        bank1 = new BankAccount("user1", "pass1", 100.0);
+        bank2 = new BankAccount("user1", "pass2", 200.0);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testAccount() {
+        assertFalse(bank1.equals(bank2));
+        bank3 = bank1;
+        assertSame(bank1, bank3);
+        assertEquals(bank1.getUsername(), bank3.getUsername());
+    }
+    
+    @Test
+    public void testBalance() {
+        Double amount = 150.0;
+        assertEquals(bank1.getBalance(), 100,0);
+        assertEquals(bank2.getBalance(), 200,0);
+        assertFalse(bank1.checkBalance(amount));
+        assertTrue(bank2.checkBalance(amount));
+        bank2.pay(amount);
+        assertEquals(bank2.getBalance(), 50,0);
+    }
+}
--- projects/BazaarWeb/web/pages/paymentSucces.jsp
+++ projects/BazaarWeb/web/pages/paymentSucces.jsp
@@ -1,19 +1,29 @@
-<%-- 
-    Document   : paymentSucces
-    Created on : 10-nov-2015, 12:25:50
-    Author     : Baya
---%>
-
+<%@page import="classes.domain.OrderItem"%>
+<%@page import="classes.domain.Cart"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Succes</title>
-    </head>
-    <body>
-        <h1>Payment success!</h1>
-        <p><%= request.getParameter("TOKEN") %></p>
-                
-    </body>
-</html>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@include file="/pages/langInclude.jsp" %>
+
+<c:set var="title"> <fmt:message key="payment.title" /></c:set>
+
+<%
+    Cart cart = Cart.readCartFromCookies(request);
+    if (cart != null) {
+        request.setAttribute("orders", cart.getOverview());
+        request.setAttribute("orderCount", cart.getOverview().length);
+        request.setAttribute("cart", cart);
+        request.setAttribute("orders", new OrderItem[]{});
+        request.setAttribute("orderCount", 0);
+        request.setAttribute("cart", new Cart());
+    } else {
+        request.setAttribute("orders", new OrderItem[]{});
+        request.setAttribute("orderCount", 0);
+        request.setAttribute("cart", new Cart());
+    }
+%>
+
+<t:MasterPageContent title="${title}">
+    <jsp:body>
+        <h1><fmt:message key="payment.success"></fmt:message></h1>
+    </jsp:body>
+</t:MasterPageContent>
