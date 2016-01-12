1efdc1610b2419f06e535613a5bf0ccd9dc14e8a
Anton van Dijk <anton.van.dijk@live.nl>
2016-01-12 12:06:28 +0100

    Fixed account id 0 when using Account.fromId

--- projects/BazaarWeb/src/classes/database/QueryBuilder.java
+++ projects/BazaarWeb/src/classes/database/QueryBuilder.java
@@ -1,11 +1,20 @@
 package classes.database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QueryBuilder {
     private Connection connection;
     private boolean wasAutoCommit;
+
+    private List<PreparedStatement> preparedStatements;
+
+    private QueryBuilder() {
+        preparedStatements = new ArrayList<>();
+    }
 
     public static QueryBuilder create() throws SQLException {
         QueryBuilder builder = new QueryBuilder();
@@ -16,7 +25,11 @@ public class QueryBuilder {
     }
 
     public void addStatement(String statement, Object... args) throws SQLException {
-
+        PreparedStatement prepare = connection.prepareStatement(statement);
+        for (int i = 0; i < args.length; i++) {
+            prepare.setObject(i + 1, args[i]);
+        }
+        preparedStatements.add(prepare);
     }
 
     public void commit() {
