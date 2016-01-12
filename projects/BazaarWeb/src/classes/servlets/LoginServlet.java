9fe1f7659e4b0ce032804786259ddfd028be7323
spmvanmierlo <spm.vanmierlo@student.fontys.nl>
2016-01-12 14:16:52 +0100

    Verkeerd inloggen gefixt!

--- projects/BazaarWeb/src/classes/servlets/LoginServlet.java
+++ projects/BazaarWeb/src/classes/servlets/LoginServlet.java
@@ -8,16 +8,14 @@ package classes.servlets;
 import classes.domain.LoginStatus;
 import classes.domain.Session;
 import classes.domain.models.Account;
-import classes.domain.models.Photographer;
 import classes.servlets.base.BaseHttpServlet;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 /**
  *
@@ -92,6 +90,7 @@ public class LoginServlet extends BaseHttpServlet {
         } finally {
             // Clear redirect data.
             session.setAttribute("redirecturl", null);
+            response.sendRedirect("pages/login.jsp");
         }
     }
 
